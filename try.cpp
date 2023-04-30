0b5bb9db3fd98b849cc57025f2c3dbd6643cc4f1
Indrajeet Shelake <indravshelake95@gmail.com>
2023-04-30 20:37:10 +0530

    structure added

--- try.cpp
+++ try.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int32_t main() {
 
-    
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<int> a(n);
+        for(auto &i:a) cin>>i;
+    }
 
     return 0;
 }
